2e3b2e1d123183733ba005c3ab4c18a54759a12e
nkolomiika <kolomiec_ns_0919@1511.ru>
2025-03-05 10:14:19 +0300

    simple edits

--- src/main/java/org/example/automanager/services/InaccuracyService.java
+++ src/main/java/org/example/automanager/services/InaccuracyService.java
@@ -1,6 +1,7 @@
 package org.example.automanager.services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.automanager.model.Inaccuracy;
 import org.example.automanager.repository.CarRepository;
 import org.example.automanager.repository.InaccuracyRepository;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InaccuracyService {
     private final InaccuracyRepository inaccuracyRepository;
+
+    public void createInaccuracy(Inaccuracy inaccuracy) {
+        inaccuracyRepository.save(inaccuracy);
+    }
 }
