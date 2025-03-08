f00cf2593666d2e1da5a629e82ff27360d02d8b5
ITerNik <ternik2705@list.ru>
2025-03-08 23:51:01 +0300

    Added routing and error handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,34 @@
 import { Button, Container, Typography } from "@mui/material";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import {ErrorPage} from "./pages/error-page";
 
-function App() {
+const App = () => <RouterProvider router={router}/>;
+
+const Page = () => {
     return (
         <Container className="flex flex-col items-center justify-center min-h-screen text-center">
             <Typography variant="h3" className="mb-4">
                 Ну типо умный автосервис
             </Typography>
-            <Button variant="contained" color="primary" onClick={() => alert('Yeah!')}>
+            <Button variant="contained" color="primary" onClick={alert}>
                 Начать
             </Button>
         </Container>
     );
 }
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Outlet/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: "",
+                element: <Page />,
+            },
+        ]
+    }
+    ]);
 
 export default App;
